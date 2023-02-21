aec228d6e1cd83af9355dfb134be41daa2e0a815
TJ Maynes <tj@tjmaynes.com>
2023-02-20 20:52:19 -0600

    feat(exercises): add first exercise

---
+++ src/exercises/findBinary.spec.ts
@@ -0,0 +1,31 @@
+import { Queue } from '../data-structures/queue/Queue'
+
+const findBinary = (n: number): string[] => {
+  const result: string[] = []
+
+  const queue = new Queue<string>()
+  queue.enqueue('1')
+
+  for (let i = 0; i < n; i++) {
+    queue.dequeue()
+
+    const currentBinary = queue.getFront()
+    if (currentBinary != null) {
+      result.push(currentBinary)
+
+      queue.enqueue(`${currentBinary}0`)
+      queue.enqueue(`${currentBinary}1`)
+    }
+  }
+
+  return result
+}
+
+describe('#findBinary', () => {
+  describe('when given a number', () => {
+    it('should return the binary-format for the number', () => {
+      expect(findBinary(3)).toStrictEqual(['1', '10', '11'])
+      expect(findBinary(5)).toStrictEqual(['1', '10', '11', '100', '101'])
+    })
+  })
+})
